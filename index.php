1db536559b92d659eceed76d9e45e2fd2e7b48bc
anneliev <annelieviklund@gmail.com>
2017-05-01 08:45:09 +0200

    your index.php

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
-<?php
-	include "error.php";
-	include "partials/header.php";
-	include "partials/blog.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>simpleCMS</title>
+</head>
+<body>
+	<p> WORKING </p>
 
-	include "partials/footer.php";
-
+	<?php 
+	include 'error.php';
+include 'database.php';
+include 'Users.php';
+include 'login.php';
+$testvaribale = new Users($pdo);
+$users=$testvaribale->getAllFrom();
+foreach ($users as $key){
+	echo "<br><br> USERSNAME:" . $key["username"] . "<br> Password: " . $key["password"] . "<br> Admin: " . $key["isAdmin"] . "<br>";
+		
 	
+}
+	?>
+</body>
+</html>
