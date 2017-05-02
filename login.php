2cf071b2d638a49b6f14b136ec22e46114a2b372
Oscar Gren <grenen89at-signgmail.com>
2017-05-02 13:30:18 +0200

    addd header

--- login.php
+++ login.php
@@ -3,27 +3,33 @@
 <head>
 	<meta charset="UTF-8">
 	<title>LogIn</title>
+	<link rel="stylesheet" href="css/bootstrap/css/bootstrap.css">
 </head>
 <body>
-<h1> Create New User </h1>
+	<div class="container-fluid">
+		<div class="col-12 col-sm-auto">
+			<h1> Create New User </h1>
+			<form action="post.php" method="POST">
+				<input placeholder="Username" type="text" name="username">
+				<input placeholder="Password" type="text" name="password">
+				<input type="submit">
+			</form>
 
-	<?php 
-include 'error.php';
-include 'database.php';
-include 'Users.php';
+			<?php 
+			include 'error.php';
+			include 'database.php';
+			include 'Users.php';
 
-$testvaribale = new Users($pdo);
-$users=$testvaribale->getAllFrom();
-foreach ($users as $key){
-	echo "<br><br> USERSNAME:" . $key["username"] . "<br> Password: " . $key["password"] . "<br> Admin: " . $key["isAdmin"] . "<br>";
-		
-	
-}
-	?>
-	<form action="post.php" method="POST">
-    <input placeholder="Username" type="text" name="username">
-    <input placeholder="Password" type="text" name="password">
-    <input type="submit">
-  </form>
+			$testvaribale = new Users($pdo);
+			$users=$testvaribale->getAllFrom();
+			foreach ($users as $key){
+				echo "<br><br> USERSNAME:" . $key["username"] . "<br> Password: " . $key["password"] . "<br> Admin: " . $key["isAdmin"] . "<br>";
+				
+				
+			}
+			?>
+
+		</div>
+	</div>
 </body>
 </html>
--- post.php
+++ post.php
@@ -6,6 +6,7 @@ include 'newuser.php';
 $hashed=password_hash($_POST["password"], PASSWORD_DEFAULT);
 $new = new Newuser($pdo);
 $new->createNew($_POST["username"],$hashed);
+header("Location:login.php")
 
 
 ?>
