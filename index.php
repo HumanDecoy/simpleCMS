db856debef634b6548bbf13d4db9ad4dffeeab4f
Oscar Gren <grenen89at-signgmail.com>
2017-05-02 12:02:13 +0200

    structure

--- index.php
+++ index.php
@@ -5,20 +5,6 @@
 	<title>simpleCMS</title>
 </head>
 <body>
-	<p> WORKING </p>
 
-	<?php 
-	include 'error.php';
-include 'database.php';
-include 'Users.php';
-include 'login.php';
-$testvaribale = new Users($pdo);
-$users=$testvaribale->getAllFrom();
-foreach ($users as $key){
-	echo "<br><br> USERSNAME:" . $key["username"] . "<br> Password: " . $key["password"] . "<br> Admin: " . $key["isAdmin"] . "<br>";
-		
-	
-}
-	?>
 </body>
 </html>
--- login.php
+++ login.php
@@ -6,6 +6,21 @@
 </head>
 <body>
 <h1> Create New User </h1>
+	<p> WORKING </p>
+
+	<?php 
+	include 'error.php';
+include 'database.php';
+include 'Users.php';
+include 'login.php';
+$testvaribale = new Users($pdo);
+$users=$testvaribale->getAllFrom();
+foreach ($users as $key){
+	echo "<br><br> USERSNAME:" . $key["username"] . "<br> Password: " . $key["password"] . "<br> Admin: " . $key["isAdmin"] . "<br>";
+		
+	
+}
+	?>
 	<form action="post.php" method="POST">
     <input placeholder="Username" type="text" name="username">
     <input placeholder="Password" type="text" name="password">
