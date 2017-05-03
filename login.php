19bfdaeb217c7d020eb06be6b1334b0872600e1a
Oscar Gren <grenen89at-signgmail.com>
2017-05-03 13:01:34 +0200

    small fixes

--- login.php
+++ login.php
@@ -20,45 +20,22 @@ include 'error.php';
 include 'database.php';
 include 'Users.php';
 
-			/*$testvaribale = new Users($pdo);
-			$users=$testvaribale->getAllFrom();
-			foreach ($users as $key){
-				echo "<br><br> USERSNAME:" . $key["username"] . "<br> Password: " . $key["password"] . "<br> Admin: " . $key["isAdmin"] . "<br>";
 
+?>
 
-			}*/
-			?>
+<div class="container">
 
-			<div class="container">
+	<form action="trylogin.php" method="POST" class="form-signin">
+		<h2 class="form-signin-heading">Please sign in</h2>
+		
+		<input id="inputUsername" class="form-control"  name="username" placeholder="Username"  required autofocus>
+		<label for="inputPassword" class="sr-only">Password</label>
+		<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
 
-				<form action="trylogin.php" method="POST" class="form-signin">
-					<h2 class="form-signin-heading">Please sign in</h2>
-				
-					<input id="inputUsername" class="form-control"  name="username" placeholder="Username"  required autofocus>
-					<label for="inputPassword" class="sr-only">Password</label>
-					<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+		<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+	</form>
 
-					<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-				</form>
-
-			</div>
-
-
-
-<!--
-	<div class="container-fluid">
-		<div class="col-12 col-sm-auto ">
-			<div class="col-12 col-sm-auto"><h1> Log In </h1></div>
-			<form action="trylogin.php" method="POST">
-				<div class="col-12 col-sm-auto"><input placeholder="Username" type="text" name="username"></div>
-				<div class="col-12 col-sm-auto"><input placeholder="Password" type="text" name="password"></div>
-				<div class="col-12 col-sm-auto"><input class="btn-info" type="submit"></div>
-			</form>
-		</div>
-	</div>
-
--->
-
+</div>
 
 
 </body>
