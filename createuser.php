1d10225393cf8f2fa6ad8b437654c2633d1c8b0a
Oscar Gren <grenen89at-signgmail.com>
2017-05-03 11:28:09 +0200

    added css for login and create user

--- createuser.php
+++ createuser.php
@@ -4,31 +4,28 @@
 	<meta charset="UTF-8">
 	<title>LogIn</title>
 	<link rel="stylesheet" href="css/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="css/form.css">
 </head>
 <body>
-	<div class="container-fluid">
-		<div class="col-12 col-sm-auto">
-			<h1> Create New User </h1>
-			<form action="post.php" method="POST">
-				<div class="col-12 col-sm-auto"><input placeholder="Username" type="text" name="username"></div>
-				<div class="col-12 col-sm-auto"><input placeholder="Password" type="text" name="password"></div>
-				<div class="col-12 col-sm-auto"><input class="btn-info" type="submit"></div>
-			</form>
-	</div>
-	</div>
-			<?php 
-			include 'error.php';
-			include 'database.php';
-			include 'Users.php';
+<?php 
+session_start();
+?>
 
-			$testvaribale = new Users($pdo);
-			$users=$testvaribale->getAllFrom();
-			foreach ($users as $key){
-				echo "<br><br> USERSNAME:" . $key["username"] . "<br> Password: " . $key["password"] . "<br> Admin: " . $key["isAdmin"] . "<br>";
+			<div class="container">
+
+				<form action="post.php" method="POST" class="form-signin">
+					<h2 class="form-signin-heading">Create new User</h2>
+				
+					<input id="inputUsername" class="form-control"  name="username" placeholder="Username"  required autofocus>
+					<label for="inputPassword" class="sr-only">Password</label>
+					<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+
+					<button class="btn btn-lg btn-primary btn-block" type="submit">Create User</button>
+				</form>
+
+			</div>
 
 
-			}
-			?>
 
 	
 </body>
