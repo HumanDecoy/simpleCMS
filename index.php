967be41081bae43284c7c3bea1e84f6c5a517af9
Oscar Gren <grenen89at-signgmail.com>
2017-05-04 10:03:00 +0200

    Still working on navbar- saving

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-		<a class="navbar-brand" href="#">Elieos</a>
+		<a class="navbar-brand" href="#">BloggPortal</a>
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
@@ -24,13 +24,21 @@
 				<li class="nav-item">
 					<a class="nav-link disabled" href="#">Disabled</a>
 				</li>
-				
-				
+
+
 			</ul>
-			<ul class="nav navbar-nav navbar-right">
-				<li class="nav-item">
-					<a class="nav-link" href="login.php">Login</a>
-				</li>
+			<ul class="nav navbar-nav navbar-right">	
+
+				<?php
+				if ($_SESSION["loggedIn"] === true){
+					echo"<li class='nav-item'>
+					<a class='nav-link' href='index.php'>Logout</a>
+				</li>";}
+				else{
+					echo"<li class='nav-item'>
+					<a class='nav-link' href='login.php'>Login</a>
+				</li>";
+				} ?>
 			</ul>
 			<form class="form-inline my-2 my-lg-0">
 			</form>
