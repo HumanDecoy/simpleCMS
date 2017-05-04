adc85c3406f5d7abef43bf4841685f6972a83ea0
Oscar Gren <grenen89at-signgmail.com>
2017-05-04 09:39:42 +0200

    added navbar, still working on it

--- index.php
+++ index.php
@@ -3,21 +3,54 @@
 <head>
 	<meta charset="UTF-8">
 	<title>simpleCMS</title>
+	<link rel="stylesheet" href="css/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-<?php session_start() ?>
+	<?php session_start() ?>
 
-<h1>DIS IS FIRSTPAGE</h1>
+	<!-- NAVBAR -->
+	<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<a class="navbar-brand" href="#">Elieos</a>
 
-<?php 
-if ($_SESSION['loggedIn'] === true){
-	echo "<h2> Welcome " . $_SESSION['username'] . "!</h2>";
-} 
-if($_SESSION['admin'] === true) {
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#">Disabled</a>
+				</li>
+				
+				
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li class="nav-item">
+					<a class="nav-link" href="login.php">Login</a>
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0">
+			</form>
+		</div>
+	</nav>
+	<!-- END NAVBAR -->
+
+	<h1>DIS IS FIRSTPAGE</h1>
+
+	<?php 
+	if ($_SESSION['loggedIn'] === true){
+		echo "<h2> Welcome " . $_SESSION['username'] . "!</h2>";
+	} 
+	if($_SESSION['admin'] === true) {
 		echo "<h3> overlord </h3>";
 	}
 	
-?>
+	?>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/jsBootstrap/bootstrap.js"></script>
 
 
 </body>
