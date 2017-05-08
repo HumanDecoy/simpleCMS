1f90488bb80df783051be42fdb2a7353a107f66b
HumanDecoy <grenen89@gmail.com>
2017-05-08 14:21:14 +0200

    idk

--- navbar.php
+++ navbar.php
@@ -4,7 +4,7 @@
 		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-		<a class="navbar-brand" style="font-family: 'Love Ya Like A Sister', cursive;"  href="index.php">Blöggportaelen</a>
+		<a class="navbar-brand" style="font-family: 'Love Ya Like A Sister', cursive;"  href="/simplecms/index.php/">Blöggportaelen</a>
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
