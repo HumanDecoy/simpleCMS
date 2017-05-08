0437817769bd7230a7b4045e712a8980c91af33d
HumanDecoy <grenen89@gmail.com>
2017-05-08 12:31:27 +0200

    things

--- partials/blog.php
+++ partials/blog.php
@@ -1,47 +1,47 @@
 <?php
-	session_start();
+session_start();
 
-	include "header.php";  
-	include "navbar.php";
-	include "error.php";
-	include "database.php";
-	include "newBlog.php";
-	include "../Users.php";
-	
-
-	$username = $_SESSION['username'];
+include "header.php";  
+include "navbar.php";
+include "error.php";
+include "database.php";
+include "newBlog.php";
+include "../Users.php";
 
 
-	$currentUser = new Users($pdo);
+$username = $_SESSION['username'];
 
-	$userData = $currentUser->getAllPosts($_SESSION['id']);
+
+$currentUser = new Users($pdo);
+
+$userData = $currentUser->getAllPosts($_SESSION['id']);
 
 
 echo "<h2>YOUR POSTS</h2><br />";
 
 foreach (array_reverse($userData) as $row)
-{ ?>
-	<div>
+	{ ?>
+<div>
 	<?php 
-		echo '
-		<h1>'.$row['title'].'</h1>
-		<p>By: '.$row['userID'].' Created at: '.$row['createdAt'].'</p>
-		<p>'.$row['post'].'</p>
-			<a href="newLike.php?postId='. $row['id'] .'">
+	echo '
+	<h1>'.$row['title'].'</h1>
+	<p>By: '.$row['userID'].' Created at: '.$row['createdAt'].'</p>
+	<p>'.$row['post'].'</p>
+	<a href="newLike.php?postId='. $row['id'] .'">
 		<button class="btn btn-lg btn-primary" type="submit" id="likeThis">Like</button></a><br /><br />
 		<a href="editPost.php?postId='. $row['id'] .'">
-		<form action="editPost.php" method="GET">
-			<button class="btn btn-lg btn-primary" type="submit" id="editThis">Edit</button></a>
-		</form><br />
-		<form action="deletePost.php" method="GET">
-			<button class="btn btn-lg btn-primary" type="submit" id="deleteThis">Delete</button>
-		</form><br /><br />
-		';
-		
-	?> 
-	</div>
-<?php 
-} 
+			
+			<button class="btn btn-lg btn-primary" type="submit" id="editThis">Edit</button>
+			<br />
+			<form action="deletePost.php" method="GET">
+				<button class="btn btn-lg btn-primary" type="submit" id="deleteThis">Delete</button>
+			</form><br /><br />
+			';
+			
+			?> 
+		</div>
+		<?php 
+	} 
 
 
-include "footer.php"; 
+	include "footer.php"; 
--- partials/newLike.php
+++ partials/newLike.php
@@ -6,10 +6,8 @@ include "../Like.php";
 session_start();
 $postLikeId=$_GET['postId'];
 $user = $_SESSION['id'];
-var_dump($user);
 $like = new Like($pdo);
 $isLiked = $like->getLike($user, $postLikeId);
-var_dump($isLiked);
 
 if ($isLiked){
 	echo' NO MORE LIKES FOR YOU MAH FRIEEEND ';
