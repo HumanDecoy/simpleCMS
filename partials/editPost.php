5ba54636816bc027904b01c14926284b4c151b19
HumanDecoy <grenen89@gmail.com>
2017-05-09 13:01:03 +0200

    dont send

--- partials/editPost.php
+++ partials/editPost.php
@@ -1,12 +1,12 @@
 <?php
   
-  include "../class/EditBlogPost.php";
+ include "../class/EditBlogPost.php";
   $currentId = $_SESSION['id'];
   $thePostId=$_GET['postId'];
 
-  $currentPost = new EditBlogPost($pdo);
+ $currentPost = new EditBlogPost($pdo);
   
-  $thePost = $currentPost->getPost($thePostId);
+ $thePost = $currentPost->getPost($thePostId);
 
 
 foreach ($thePost as $row)
@@ -15,7 +15,7 @@ foreach ($thePost as $row)
 <div class="container">
   <div class="row row-centered">
   
-  <form action="/simplecms/partials/postToEdit.php" method="POST">
+ <form action="/simplecms/partials/postToEdit.php" method="POST">
     <label>Title</label><br />
       <input type="text" name="editTitle" value="<?php echo $row['title'] ?>"><br />
       <label>Blogpost</label><br />
@@ -24,8 +24,8 @@ foreach ($thePost as $row)
       <input class="btn btn-lg btn-primary" type="submit" name="sumbit" value="Edit"></button><br />
   </form><br /><br />
 
-  </div>
+ </div>
 </div>
   
 
-<?php } 
+<?php }
--- partials/navbar.php
+++ partials/navbar.php
@@ -1,47 +1,48 @@
 
-	<!-- NAVBAR -->
-	<nav class="navbar navbar-toggleable-md navbar-light bg-faded" style="background-color: #b5d7e5;">
-		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<a class="navbar-brand" style="font-family: 'Love Ya Like A Sister', cursive;"  href="/simplecms/index.php/">Blöggportaelen</a>
+<!-- NAVBAR -->
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded" style="background-color: #b5d7e5;">
+	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<a class="navbar-brand" style="font-family: 'Love Ya Like A Sister', cursive;"  href="/simplecms/index.php/">Blöggportaelen</a>
 
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav mr-auto">
-				<li class="nav-item">
-					<a class="nav-link" href="/simplecms/index.php">Home <span class="sr-only">(current)</span></a>
-				</li>
-				<?
-				if ($_SESSION["loggedIn"] === true){
-					echo"<li class='nav-item'>
-					<a class='nav-link' href='/simplecms/partials/blog.php'>New Blogpost</a>
-				</li>";}
-				?>
-			</ul>
-			<ul class="nav navbar-nav navbar-right">	
-				<?
-				if($_SESSION["admin"]===true){
-					echo"<li class='nav-item'>
-					<a class='nav-link' href='/simplecms/partials/admin.php'>Admin</a>";
-				}
-				
-				if ($_SESSION["loggedIn"] === true){
-					echo"<li class='nav-item'>
-					<a class='nav-link' href='/simplecms/partials/logout.php'>Logout</a>
-				</li>";}
-				else{
-					echo"<li class='nav-item'>
-					<a class='nav-link' href='/simplecms/partials/login.php'>Login</a>";
-					echo"<li class='nav-item'>
-					<a class='nav-link' href='/simplecms/partials/createuser.php'>Register</a></li>";
-				}
-				
-				
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="/simplecms/index.php">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<?
+			if ($_SESSION["loggedIn"] === true){
+				echo"<li class='nav-item'>
+				<a class='nav-link' href='/simplecms/partials/blog.php'>New Blogpost</a>
+			</li>";}
+			if($_SESSION["admin"]===true){
+				echo"<li class='nav-item'>
+				<a class='nav-link' href='/simplecms/partials/admin.php'>Admin</a>";
+			}
+			?>
 
-				 ?>
-			</ul>
-			<form class="form-inline my-2 my-lg-0">
-			</form>
-		</div>
-	</nav>
-	<!-- END NAVBAR -->
+		</ul>
+		<ul class="nav navbar-nav navbar-right">	
+			<?
+
+			if ($_SESSION["loggedIn"] === true){
+				echo"<li class='nav-item'>
+				<a class='nav-link' href='/simplecms/partials/logout.php'>Logout</a>
+			</li>";}
+			else{
+				echo"<li class='nav-item'>
+				<a class='nav-link' href='/simplecms/partials/login.php'>Login</a>";
+				echo"<li class='nav-item'>
+				<a class='nav-link' href='/simplecms/partials/createuser.php'>Register</a></li>";
+			}
+
+
+
+			?>
+		</ul>
+		<form class="form-inline my-2 my-lg-0">
+		</form>
+	</div>
+</nav>
+<!-- END NAVBAR -->
