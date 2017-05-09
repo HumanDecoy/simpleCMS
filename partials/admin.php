d3a924bc597bbee0e5ca0e45ca8999e503140759
HumanDecoy <grenen89@gmail.com>
2017-05-09 12:47:48 +0200

    admin panel still

--- partials/admin.php
+++ partials/admin.php
@@ -2,7 +2,6 @@
 session_start();
 include "header.php";
 include "navbar.php";
-var_dump($_SESSION);
 if($_SESSION["admin"]==!true){
 header("Location:/simpleCMS/index.php");
 				}
@@ -13,8 +12,14 @@ header("Location:/simpleCMS/index.php");
 <h1 align="center"> Welcome to the Admin panel, <?= $_SESSION['username']; ?> </h1>
 
 
+<div class="searching">
+<form action="" method="POST" >
+<h2 align="center"> Search for User </h2>
 
-
+<input align="center" class="form-control col-md-6 offset-md-3"  name="username" placeholder="Username">
+ <button class="col-md-6 offset-md-3 btn btn-lg btn-primary" type="submit">Search</button>
+ </form>
+</div>
 
 
 
